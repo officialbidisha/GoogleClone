aeddc434f544d62d8e57f6be601332c4d8a234b0
Bidisha Das <officialbidisha1@gmail.com>
2022-06-02 18:31:34 +0530

    1.Added image result, pagination and search, 2. Completed the application

--- pages/search.js
+++ pages/search.js
@@ -1,21 +1,45 @@
 import Head from "next/head";
-import Header from "../components/Header";
 import SearchHeader from "../components/SearchHeader";
+import SearchResults from '../components/SearchResults';
+import ImageResults from "../components/ImageResult";
 
-const search = () => {
-    return(
-        <div>
-           <Head>
-               <title>Search Page</title>
-           </Head>
+import { useRouter } from "next/router";
 
-           <SearchHeader/>
-        </div>
-    )
-}
+const search = ({results}) => {
+    console.log("Result", results);
+    const router = useRouter();
+  return (
+    <div>
+      <Head>
+        <title>{router.query.term} - Search Page</title>
+      </Head>
 
-export async function getServerSideProps(){
-    
+      <SearchHeader />
+
+      {router.query.searchType === "image" ? (
+        <ImageResults results={results} />
+      ) : (
+        <SearchResults results={results} />
+      )}
+    </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+
+  const startIndex = context.query.start || "1";
+  const mockData = false;
+
+  const data = mockData? Response: await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=AIzaSyB9C2UT-2ycjeHan64-e7CWPkEviBDFLws&cx=e27a9c623921b3f07&q=${
+      context.query.term
+    }${context.query.searchType && "&searchType=image"}&start=${startIndex}`
+  ).then((res)=> res.json());
+  return{
+      props:{
+          results:data
+      }
+  }
 }
 
 export default search;
